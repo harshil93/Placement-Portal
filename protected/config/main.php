75d12dbc5fd9d22b2e1dc5901f67641b302a31f1
Harshil Lodhi <lodhi.harshil@gmail.com>
2014-04-16 22:45:17 +0530

    models created

--- protected/config/main.php
+++ protected/config/main.php
@@ -53,7 +53,7 @@ return array(
 		// uncomment the following to use a MySQL database
 		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=placement',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => 'enigma',
