43c8768734d94d134059e106774cc08c735075b5
Shobhit Chaurasia <sc.shobhit@gmail.com>
2014-04-19 01:45:13 +0530

    PR 90% done

---
+++ protected/views/placementRep/_reviewCVPart.php
@@ -0,0 +1,19 @@
+<?php
+/* @var $this PlacementRepController */
+/* @var $data PlacementRep */
+?>
+
+<div class="view">
+	<b><?php echo CHtml::encode("cv_id"); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data['st_id'])); ?>
+	<br />
+
+	<b><?php echo CHtml::encode("cv_name"); ?>:</b>
+	<?php echo CHtml::encode($data['cv_name']); ?>
+	<br />
+
+	
+
+
+</div>
+
