6f0e926c5fb4f90bfb8fc04b427674b670ba2c03
golden_ratio <j.aditya@iitg.ernet.in>
2014-04-19 19:27:55 +0530

    Create offer added

--- protected/controllers/ApplyController.php
+++ protected/controllers/ApplyController.php
@@ -4,85 +4,89 @@ class ApplyController extends Controller
 {
 	public function actionCreate($j_id, $c_id)
 	{
+        if(Yii::app()->session['role'] == 1)
+        {
+            $sqlcount =  Yii::app()->db->createCommand("select count(*) from job_profile where j_id = ".$j_id." and c_id = ".$c_id)->queryScalar();
+            $sql = "select * from job_profile where j_id = ".$j_id." and c_id = ".$c_id;
+
+            $dataProvider = new CSqlDataProvider($sql, array(
+                'db' => Yii::app()->db,
+                'keyField' => 'j_id',
+                'totalItemCount' => $sqlcount
+            ));
+
+            $sqlcount =  Yii::app()->db->createCommand("select count(*) from cv_table where st_id = ".Yii::App()->user->id)->queryScalar();
+            $sql = "select * from cv_table where st_id = ".Yii::App()->user->id;
+            $res =  Yii::app()->db->createCommand("select * from cv_table where st_id = ".Yii::App()->user->id)->queryAll();
 
 
-        $sqlcount =  Yii::app()->db->createCommand("select count(*) from job_profile where j_id = ".$j_id." and c_id = ".$c_id)->queryScalar();
-        $sql = "select * from job_profile where j_id = ".$j_id." and c_id = ".$c_id;
 
-        $dataProvider = new CSqlDataProvider($sql, array(
-        'db' => Yii::app()->db,
-        'keyField' => 'j_id',
-        'totalItemCount' => $sqlcount
-        ));
+            $cv_list = array();
 
-        $sqlcount =  Yii::app()->db->createCommand("select count(*) from cv_table where st_id = ".Yii::App()->user->id)->queryScalar();
-        $sql = "select * from cv_table where st_id = ".Yii::App()->user->id;
-        $res =  Yii::app()->db->createCommand("select * from cv_table where st_id = ".Yii::App()->user->id)->queryAll();
+            foreach($res as $item){
+                $cv_list[$item["cv_id"]] = $item["cv_name"];
+            }
 
-
-
-        $cv_list = array();
-
-        foreach($res as $item){
-            $cv_list[$item["cv_id"]] = $item["cv_name"];
+            $this->render('create',array(
+                'dataProvider'=>$dataProvider,
+                'cv_list'=>$cv_list,
+                'j_id'=>$j_id,
+                'c_id'=>$c_id,
+            ));
         }
 
-        $this->render('create',array(
-        'dataProvider'=>$dataProvider,
-        'cv_list'=>$cv_list,
-         'j_id'=>$j_id,
-         'c_id'=>$c_id,
-        ));
 	}
 
     public function actionSave($j_id,$c_id)
     {
+        if(Yii::app()->session['role'] == 1)
+        {
+            if(!(isset($_POST['cv_id']) && isset($j_id) && isset ($c_id))){
+                Yii::app()->user->setFlash('error','Error');
+                $this->redirect(array('index'));
+            }
+            $studinfo = Student::model()->findByPk(Yii::App()->user->id);
 
-        if(!(isset($_POST['cv_id']) && isset($j_id) && isset ($c_id))){
-            Yii::app()->user->setFlash('error','Error');
-            $this->redirect(array('index'));
-        }
-        $studinfo = Student::model()->findByPk(Yii::App()->user->id);
+            $dept = $studinfo->getAttribute("dept");
+            $cpi = $studinfo->getAttribute("cpi");
 
-        $dept = $studinfo->getAttribute("dept");
-        $cpi = $studinfo->getAttribute("cpi");
-
-        $sqlcount =  Yii::app()->db->createCommand("select count(*) from job_profile_branches as jpb where j_id =
+            $sqlcount =  Yii::app()->db->createCommand("select count(*) from job_profile_branches as jpb where j_id =
          ".$j_id." and c_id = ".$c_id." and dept = '".$dept."'" )->queryScalar();
 
-        $jobinfo = JobProfile::model()->find('j_id = ? and c_id = ?',array($j_id,$c_id));
+            $jobinfo = JobProfile::model()->find('j_id = ? and c_id = ? and CURRENT_TIMESTAMP>deadline',array($j_id,$c_id));
 
-        if(count($jobinfo))
-        {
-            $cutoff = $jobinfo->getAttribute("cpi_cutoff");
-
-            if($sqlcount && $cpi>=$cutoff)
+            if(count($jobinfo))
             {
-               try{
-                   $st_id = Yii::App()->user->id;
-                   $connection = Yii::App()->db;
-                   $sql = "INSERT INTO cv_table (j_id,c_id,cv_id,st_id) values(:j_id,:c_id,:cv_id,:st_id)";
-                   $command = $connection->createCommand($sql);
-                   $command->bindParam(":j_id",$j_id,PDO::PARAM_STR);
-                   $command->bindParam(":c_id",$c_id,PDO::PARAM_STR);
-                   $command->bindParam(":cv_id",$_POST['cv_id'],PDO::PARAM_STR);
-                   $command->bindParam(":st_id",$st_id,PDO::PARAM_STR);
-                   $command->execute();
-                   
-               }catch(Exception $e){
+                $cutoff = $jobinfo->getAttribute("cpi_cutoff");
 
-                exit(1);
-            }
+                if($sqlcount && $cpi>=$cutoff)
+                {
+                    try{
+                        $st_id = Yii::App()->user->id;
+                        $connection = Yii::App()->db;
+                        $sql = "INSERT INTO cv_table (j_id,c_id,cv_id,st_id) values(:j_id,:c_id,:cv_id,:st_id)";
+                        $command = $connection->createCommand($sql);
+                        $command->bindParam(":j_id",$j_id,PDO::PARAM_STR);
+                        $command->bindParam(":c_id",$c_id,PDO::PARAM_STR);
+                        $command->bindParam(":cv_id",$_POST['cv_id'],PDO::PARAM_STR);
+                        $command->bindParam(":st_id",$st_id,PDO::PARAM_STR);
+                        $command->execute();
+
+                    }catch(Exception $e){
+                        echo "Exception";
+                        exit(1);
+                    }
+                }else{
+                    Yii::app()->user->setFlash('error','sql count cpi cutoff');
+                }
+
             }else{
-                Yii::app()->user->setFlash('error','sql count cpi cutoff');
+                Yii::app()->user->setFlash('error','count 0');
+
             }
 
-        }else{
-            Yii::app()->user->setFlash('error','count 0');
-
+            $this->redirect('index.php?r=student/index');
         }
-
-        $this->redirect('index.php?r=student/index');
     }
 
 	public function actionDelete()
@@ -108,7 +112,15 @@ class ApplyController extends Controller
 			where s.st_id=a.st_id and a.j_id = jp.j_id and a.c_id = jp.c_id and s.st_id = ".Yii::App()->user->id)->queryScalar();
             $sql = "select * from student as s, apply as a, job_profile as jp
 			where s.st_id=a.st_id and a.j_id = jp.j_id and a.c_id = jp.c_id and s.st_id = ".Yii::App()->user->id;
+            $dataProvider = new CSqlDataProvider($sql, array(
+                'db' => Yii::app()->db,
+                'keyField' => 'j_id',
+                'totalItemCount' => $sqlcount
+            ));
 
+            $this->render('view',array(
+                'dataProvider'=>$dataProvider,
+            ));
         }
         else if(Yii::app()->session['role'] == 2)
         {
@@ -116,17 +128,16 @@ class ApplyController extends Controller
 			where s.st_id=a.st_id and a.j_id = jp.j_id and a.c_id = jp.c_id and jp.c_id = ".Yii::App()->user->id)->queryScalar();
             $sql = "select * from student as s, apply as a, job_profile as jp
 			where s.st_id=a.st_id and a.j_id = jp.j_id and a.c_id = jp.c_id and jp.c_id = ".Yii::App()->user->id;
+            $dataProvider = new CSqlDataProvider($sql, array(
+                'db' => Yii::app()->db,
+                'keyField' => 'j_id',
+                'totalItemCount' => $sqlcount
+            ));
+
+            $this->render('view',array(
+                'dataProvider'=>$dataProvider,
+            ));
         }
-
-		$dataProvider = new CSqlDataProvider($sql, array(
-			'db' => Yii::app()->db,
-			'keyField' => 'j_id',
-			'totalItemCount' => $sqlcount
-		));
-
-		$this->render('view',array(
-			'dataProvider'=>$dataProvider,
-		));
 
 	}
 
